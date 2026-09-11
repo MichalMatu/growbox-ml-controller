9f12d36784c5df7c25dec613136cb279cca11d70
Michal Matuszewski <90936684+MichalMatu@users.noreply.github.com>
2026-09-11 05:44:38 +0200

    Add CrowPanel board profile

---
+++ config/boards/crowpanel-esp32s3-2_9-n8r8.cmake
@@ -0,0 +1,4 @@
+# Hardware values that differ from the shared Growbox defaults.
+growbox_cache_default(GROWBOX_I2C_SDA_GPIO STRING "21" "Stage27 shared I2C SDA GPIO")
+growbox_cache_default(GROWBOX_I2C_SCL_GPIO STRING "38" "Stage27 shared I2C SCL GPIO")
+growbox_cache_default(GROWBOX_SD_POWER_GPIO STRING "42" "Stage27 SD power-enable GPIO")
