8983fdab17f5089820b7eea283abe805d4818973
Michal Matuszewski <90936684+MichalMatu@users.noreply.github.com>
2026-09-11 09:26:03 +0200

    Decouple runtime composition from transport implementation

--- src/climate/runtime/RealInputRuntimeComposition.h
+++ src/climate/runtime/RealInputRuntimeComposition.h
@@ -16,46 +16,16 @@
 #include "climate/output/OutputSupervisorExecutor.h"
 #include "climate/output/OutputSupervisorLifecycle.h"
 #include "climate/output/OutputSupervisorResolver.h"
-#include "climate/output/OutputTransport.h"
 #include "climate/rf433/Rf433OutputTransport.h"
 #include "climate/rf433/Rf433RmtFrameSender.h"
 #include "climate/rf433/Rf433RmtLoopback.h"
+#include "climate/runtime/RuntimeOutputTransport.h"
 #include "climate/runtime/Stage27RuntimeAdapters.h"
 #include "climate/runtime/Stage28MaintenanceRfTransport.h"
 #include "climate/runtime/Stage28RfDiagnostics.h"
 #include "climate/storage/Stage27TelemetryLogger.h"
 
-#include <cstdint>
-
 namespace growbox::app::climate_io::runtime {
-
-struct RuntimeExecutionStatus final {
-  bool transport_available{false};
-  bool output_ready{false};
-};
-
-class RuntimeOutputTransport final : public output::OutputTransport {
-public:
-  RuntimeOutputTransport(output::OutputTransport& real_transport,
-                         RuntimeExecutionStatus& execution_status) noexcept
-      : real_transport_(real_transport), execution_status_(execution_status) {}
-
-  output::TxResult send(const output::OutputCommand& command) noexcept override;
-
-  std::uint32_t transmitCount() const noexcept {
-    return transmit_count_;
-  }
-
-  std::uint32_t transmitErrorCount() const noexcept {
-    return transmit_error_count_;
-  }
-
-private:
-  output::OutputTransport& real_transport_;
-  RuntimeExecutionStatus& execution_status_;
-  std::uint32_t transmit_count_{0U};
-  std::uint32_t transmit_error_count_{0U};
-};
 
 class RuntimeIoOwner final {
 public:
